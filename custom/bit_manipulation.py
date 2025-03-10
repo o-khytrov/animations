29f76b35104272a7b5bd03f25cf51678c7eb4d26
Oleksandr Khytrov <o.khytrov@gmail.com>
2025-03-10 11:18:01 +0200

    Bit manipulation

--- custom/bit_manipulation.py
+++ custom/bit_manipulation.py
@@ -1,5 +1,10 @@
 from setup import *
 
+config.pixel_height = 1920
+config.pixel_width = 1080
+config.frame_height = 16.0
+config.frame_width = 9.0
+
 
 class BitManipulation(Scene):
     def math_per_character(self, expression: str, **kwargs):
@@ -31,10 +36,17 @@ class BitManipulation(Scene):
 
         a_txt_copy = a_txt.copy()
         b_txt_copy = b_txt.copy()
-        code = Code(code_string=code_str, language="python", add_line_numbers=False).next_to(expression_left_side, direction=UP)
+        code_background_config = {
+            "corner_radius": 0
+        }
+        code = Code(code_string=code_str, language="python", add_line_numbers=False, background="rectangle",
+                    background_config=code_background_config).next_to(expression_left_side,
+                                                                      direction=UP)
         title_txt = Text(title).next_to(code, direction=UP)
-        expression_group = VGroup(title_txt, a_txt, sign_txt, b_txt, eq_txt, res_txt, bin_a_text, bin_b_text, bin_res_txt, a_txt_copy, b_txt_copy, bin_xor_txt_copy, underline, code)
+        expression_group = VGroup(title_txt, a_txt, sign_txt, b_txt, eq_txt, res_txt, bin_a_text, bin_b_text,
+                                  bin_res_txt, a_txt_copy, b_txt_copy, bin_xor_txt_copy, underline, code)
         expression_group.move_to(start_point)
+        expression_group.scale(2)
 
         self.play(Write(title_txt))
         self.play(Create(code))
@@ -43,7 +55,7 @@ class BitManipulation(Scene):
         self.play(b_txt_copy.animate.become(bin_b_text))
         self.play(Create(underline))
         for i in reversed(range(len(res_bin_str))):
-            indication_color = GREEN if res_bin_str[i] == '1' else RED
+            indication_color = Green if res_bin_str[i] == '1' else Red
             self.remove(b_txt_copy[i])
             self.remove(a_txt_copy[i])
             ba = bin_a_text[i]
@@ -68,6 +80,9 @@ class BitManipulation(Scene):
     def bitwise_or(self, a, b, start_point):
         self.bitwise_operation(a, b, lambda x, y: x | y, "\lor", start_point, "OR", "|")
 
+    def bitwise_shift_right(self, a, b, start_point):
+        self.bitwise_operation(a, b, lambda x, y: x >> y, "\gg", start_point, "Right Shifting ", ">>")
+
     def decimal_to_binary_conversion(self):
         previous = None
         for number in range(1, 10):
@@ -79,15 +94,18 @@ class BitManipulation(Scene):
             self.play(Write(decimal_text), run_time=0.3)
 
             binary_string = f"{number:04b}"
-            self.play(decimal_text.copy().animate.become(MathTex(binary_string).next_to(decimal_text, direction=RIGHT)), run_time=0.3)
+            self.play(decimal_text.copy().animate.become(MathTex(binary_string).next_to(decimal_text, direction=RIGHT)),
+                      run_time=0.3)
 
             previous = decimal_text
 
     def construct(self):
 
         # self.decimal_to_binary_conversion()
-
+        #text = Text("101010", font="Monaco").scale(2)
+        # self.play(Write(text))
         self.bitwise_or(5, 7, ORIGIN)
         self.bitwise_and(5, 7, ORIGIN)
         self.bitwise_xor(5, 7, ORIGIN)
+        # self.bitwise_shift_right(3, 1, ORIGIN)
         self.wait()
